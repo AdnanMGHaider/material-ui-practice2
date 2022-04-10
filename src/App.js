89afd7a5482afe2ac4b58868f84167445c0bf4e1
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-10 21:45:18 +0600

    createTheme | ThemeProvider | SearchAppBar | etc..

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import TourCard from "./components/TourCard";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
+import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour) => (
+                <TourCard tour={tour} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,39 +1,66 @@
 import React from "react";
-import { Paper, Grid, Typography, Box, Rating } from "@mui/material";
+import { Paper, Grid, Typography, Box, Rating, createTheme, ThemeProvider } from "@mui/material";
 import beach from "../assets/beach.jpg";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
-const TourCard = () => {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
+
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={6}>
-        <img className="img" src={beach} alt="" />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Breathtaking waterfalls
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <AccessTimeIcon sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={6}>
+          <img className="img" src={tour.image} alt="" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              {tour.name}
             </Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <AccessTimeIcon sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.duration} hours
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={1}>
+              <Rating name="read-only" value={tour.rating} readOnly precision={0.5} size="small" />
+              <Typography variant="body2" component="p" margin={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body3" component="p" margin={1.5}>
+                ({tour.numberOfReviews} reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From ${tour.price}
+              </Typography>
+            </Box>
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={1}>
-            <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small" />
-            <Typography variant="body2" component="p" margin={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" margin={1.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From $150
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
