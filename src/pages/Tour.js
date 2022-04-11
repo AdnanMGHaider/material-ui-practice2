c07321c02065b6c7b4624b752d4664e56bc406a9
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-11 12:34:17 +0600

    Imported and used MUI components like ImageList, ImageListItem, Accordion, BasicNavigation and BasicModal

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Paper, BottomNavigation } from "@mui/material";
 import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordion";
+import BasicModal from "../components/Modal";
 
 const Tour = () => {
   return (
@@ -12,6 +14,26 @@ const Tour = () => {
         <img src="https://media.timeout.com/images/105124791/750/422/image.jpg" alt="" height={325} />
         <ImageCollage />
       </Box>
+      <Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography variant="paragraph" component="p" marginTop={3}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+        </Typography>
+      </Box>
+      <Box marginBottom={10}>
+        <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+          Frequently Asked Questions
+        </Typography>
+        <CustomizedAccordions />
+      </Box>
+      <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation>{/* <BasicModal /> */}</BottomNavigation>
+      </Paper>
     </Container>
   );
 };
